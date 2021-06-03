3d1cd4724ae4cb49ac0b04b079924837353eb65f
Paasha <75585440+paashag@users.noreply.github.com>
2021-06-03 00:44:16 -0700

    Update dispenser.ino

--- dispenser.ino
+++ dispenser.ino
@@ -1,31 +1,39 @@
 #include <Servo.h>
-Servo dispenseMotor;
+Servo dispenseMotor
+myservo.attach(6969);
 
 #include <Ultrasonic.h>
 Ultrasonic ultrasonic(69, 69);
 
-const int trigPin = 420;
-const int echoPin= 69;
-
+const int shoutPin = 420;
+const int listenPin = 69;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  dispenseMotor.attach(6969);
+  pinMode(shoutPin, OUTPUT);
+  pinMode(listenPin, INPUT);
+  bool handInRange = false;
+
 }
 
 void loop() {
+  int distanceCM = ultrasonic.read(CM);
   
-  int cm = ultrasonic.read();
-  Serial.print("Distance: ");
-  Serial.print(cm); 
-  Serial.print("cm");
+  if (handInRange = true) {
     
-  if (cm < 12) {
+  if (distanceCM < 12) {
     dispenseMotor.write(90);
-    delay(2000);
+    delay(600);
     dispenseMotor.write(0);
-  }
-}
+    bool handInRange = true;
+  } else if (distanceCM > 12) { 
+    bool handInRange = false;
+    } else {
+      cout << "Error: Value not found" << endl;
+      return 0;
+    }
+    
+} else { }
 
+  delay(125); 
+}
