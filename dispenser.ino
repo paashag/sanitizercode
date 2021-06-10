cd1c3d6c1a871c9f0a425ed7f1ad7ba1b3ba4870
Beluguy <75653877+Beluguy@users.noreply.github.com>
2021-06-09 18:02:55 -0700

    Update dispenser.ino

--- dispenser.ino
+++ dispenser.ino
@@ -1,39 +1,77 @@
+// Storage Library
+#include <EEPROM.h>
+
 #include <Servo.h>
-Servo dispenseMotor
-myservo.attach(6969);
+Servo dispenseMotor;
+const int servoPin = 3;
 
 #include <Ultrasonic.h>
-Ultrasonic ultrasonic(69, 69);
+const int shoutPin = 9;
+const int listenPin = 8;
+Ultrasonic ultrasonic(shoutPin, listenPin, 6900UL);
 
-const int shoutPin = 420;
-const int listenPin = 69;
+const int resetPin = 12;
+int currentErrors = EEPROM.read(0);
+bool handInRange = false;
 
+
+// Startup Sequence
 void setup() {
   Serial.begin(9600);
-  pinMode(shoutPin, OUTPUT);
-  pinMode(listenPin, INPUT);
-  bool handInRange = false;
+  digitalWrite(resetPin, HIGH);
+  pinMode(resetPin, OUTPUT);
+  delay(200);
+  Serial.print("Errors: ");
+  Serial.println(currentErrors);
 
+  if (currentErrors == 5) {
+// Wait 20 seconds before starting up after errors
+    Serial.println("Repeated crashes, resetting in 20s");
+    delay(20000);
+    int newErrors = 0;
+    EEPROM.write(0, newErrors);
+    digitalWrite(resetPin, LOW);
+  } 
+  else {
+  dispenseMotor.attach(servoPin);
+  } 
 }
 
+// Looping Process
 void loop() {
-  int distanceCM = ultrasonic.read(CM);
+  int cm = ultrasonic.read();
+  Serial.print(cm); 
+  Serial.println("cm");
   
-  if (handInRange = true) {
-    
-  if (distanceCM < 12) {
+  if (cm < 25) { 
     dispenseMotor.write(90);
-    delay(600);
-    dispenseMotor.write(0);
-    bool handInRange = true;
-  } else if (distanceCM > 12) { 
-    bool handInRange = false;
-    } else {
-      cout << "Error: Value not found" << endl;
-      return 0;
+    bool handInRange = true; 
+    delay(200);
+  }
+  else if (cm > 25) { 
+    bool handInRange = false;  
+  }
+  else {
+      Serial.println("Error: Value not found, check connection");
+      int newErrors = currentErrors + 1;
+      EEPROM.write(0, newErrors);
+      digitalWrite(resetPin, LOW);
     }
     
-} else { }
-
-  delay(125); 
+while (handInRange = true) { 
+  int cm = ultrasonic.read();
+  Serial.print(cm); 
+  Serial.println("cm");
+  if (cm > 25) { 
+    dispenseMotor.write(0);
+    delay(1000);
+    bool handInRange = false; 
+    break;
+  }
+  else { 
+    break;
+    }
+    delay(150);
+  }
+return;
 }
